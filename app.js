de0767fa1bc05d37ec0d32fc7a347ba22ef80388
syam <syam@syams-MacBook-Pro.local>
2017-12-25 23:32:29 +0800

    modified

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // packages
 var restify = require('restify');
 var builder = require('botbuilder');
+var http = require('http');
+
 
 //setup server
 var server = restify.createServer();
@@ -10,8 +12,11 @@ server.listen(process.env.PORT || 3978,function()
     console.log('%s listening to %s',server.name,server.url);
 });
 
+var myAppId = process.env.APP_ID || "Missing your app ID";
+var myAppSecret = process.env.APP_SECRET || "Missing your app secret";
+
 //Create chat bot
-var connector = new builder.ChatConnector({appId: '03aafc3e-a722-4a1a-8036-c7c2263e0e46',appPassword: 'k^w@OCHb}dSRnE*W'});
+var connector = new builder.ChatConnector({appId: myAppId,appPassword: myAppSecret});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages',connector.listen());
 
