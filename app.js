44f62cb3d9cd4d0f3678afaba3a059517650226e
syam <syam@syams-MacBook-Pro.local>
2017-12-26 20:52:06 +0800

    debug1

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ server.listen(process.env.PORT || 3978,function()
     console.log('%s listening to %s',server.name,server.url);
 });
 
-var myAppId = process.env.APP_ID || "Missing your app ID";
-var myAppSecret = process.env.APP_SECRET || "Missing your app secret";
+var myAppId = "03aafc3e-a722-4a1a-8036-c7c2263e0e46";//process.env.APP_ID || "Missing your app ID";
+var myAppSecret = "k^w@OCHb}dSRnE*W";//process.env.APP_SECRET || "Missing your app secret";
 
 //Create chat bot
 var connector = new builder.ChatConnector({appId: myAppId,appPassword: myAppSecret});
